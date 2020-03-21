2cc94f444c201b26d057545a2bad2b0c51fcbc08
Jordan Melidor-Fuxis <jordan.melidor@hotmail.fr>
2020-03-21 17:09:10 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-  
+  swimming = ["fly", "back", "breast", "free"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  swimming 
 end
 
 def add_element_to_start_of_array(array, element)
